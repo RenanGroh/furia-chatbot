dea3e180f9236f68652edf7f972f06f9611d6087
RenanGroh <renangroh@gmail.com>
2025-05-03 23:32:59 -0300

    fix gh pages

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,16 +1,14 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  base: '/furia-chatbot/',
+  // ======================================
+  server: { 
     port: 5173,
     strictPort: true,
-    base: "/furia-chatbot/",
-    server: {
-      port: 5173,
-      strictPort: true,
-    },
   },
 });
